faa650cb102e209e47cb2166f7987c410111f778
garedunord21 <garedunord21@gmail.com>
2024-09-05 10:54:04 +0900

    2024.09.05 10:50

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router';
-import './style.css'
+import './style.css';
 
 // component: 로고 컴포넌트 //
 function Logo() {
 
     // render: 로고 컴포넌트 렌더링 //
     return (
-        <div id='layout-log'></div>
+        <div id='layout-logo'>
+            <div className='box'>
+                <div className='title'>시니케어</div>
+                <div className='icon'></div>
+            </div>
+        </div>
     );
 
 }
@@ -17,7 +22,10 @@ function Top() {
 
     // render: 상단 컴포넌트 //
     return (
-        <div id='layout-top'></div>
+        <div id='layout-top'>
+            <div className='path'>고객 관리</div>
+            <div className='button second'>로그아웃</div>
+        </div>
     );
 
 }
@@ -25,7 +33,7 @@ function Top() {
 // component: 좌측 네비게이션 컴포넌트 //
 function SideNavigation() {
 
-    // render: 죄측 네비게이션 컴포넌트 //
+    // render: 좌측 네비게이션 컴포넌트 //
     return (
         <div id='layout-side-navigation'></div>
     );
